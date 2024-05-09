35cea9e2d85ca494acccb99f800dbbd1440adc12
YYovchev123 <jonimi@abv.bg>
2024-05-09 11:48:26 +0300

    lib_natspedc

--- src/libraries/EthAddressLib.sol
+++ src/libraries/EthAddressLib.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity 0.8.20;
 
+/// @title EthAddressLib
+/// @author YovchevYoan
+/// @notice A library for fetching eth Address
+/// @dev It includes function returning eth Address
 library EthAddressLib {
     /**
      * @dev returns the address used within the protocol to identify ETH
